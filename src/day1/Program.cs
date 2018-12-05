a5d3ce64a4fabf532ed9c06e94642dbcf8e5867f
Cam Warner <cameroncwarner@gmail.com>
2018-12-04 19:22:12 -0500

    Add day1 part1 solution

---
+++ src/day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using common;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(InputReader reader = new InputReader("input.txt"))
+            {
+                Console.WriteLine(reader.GetLines().Select(x => int.Parse(x)).Sum());
+            }
+        }
+    }
+}

97a90efa3241346921127f0455e4a57b07ce98e1
Cam Warner <cameroncwarner@gmail.com>
2018-12-04 19:21:45 -0500

    Add scaffolding and InputReader

---
+++ src/common/InputReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace common
+{
+    public class InputReader : IDisposable
+    {
+        private StreamReader _reader;
+
+        public InputReader(string filePath)
+        {
+            _reader = new StreamReader(filePath);
+        }
+
+        public void Dispose()
+        {
+            _reader.Dispose();
+        }
+
+        public IEnumerable<string> GetLines()
+        {
+            while(!_reader.EndOfStream)
+            {
+                yield return _reader.ReadLine();
+            }
+        }
+
+        public string GetNextLine()
+        {
+            if(_reader.EndOfStream)
+            {
+                return null;
+            }
+
+            return _reader.ReadLine();
+        }
+    }
+}

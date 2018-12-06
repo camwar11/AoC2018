d76211e2277d3bfe93069ac369b2a93f21a4db9c
Cam Warner <cameroncwarner@gmail.com>
2018-12-05 22:45:50 -0500

    Add day 5 part 1 solution

--- src/day5/Program.cs
+++ src/day5/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using common;
 
 namespace day5
 {
@@ -6,7 +9,31 @@ namespace day5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var reader = new InputReader("input.txt"))
+            {
+                string polymer = reader.GetNextLine();
+
+                Stack<char> result = new Stack<char>();
+
+                foreach (char nextChar in polymer)
+                {
+                    char prev;
+                    if(!result.TryPeek(out prev) || !AreOppositePolarity(prev, nextChar))
+                    {
+                        result.Push(nextChar);
+                        continue;
+                    }
+
+                    result.Pop();
+                }
+
+                Console.WriteLine("Part1: {0}", result.Count);
+            }
+        }
+
+        static bool AreOppositePolarity(char one, char two)
+        {
+            return one != two && char.ToLower(one) == char.ToLower(two);
         }
     }
 }

7f12920ada2ab08c734412931bd7cc6905fc6837
Cam Warner <cameroncwarner@gmail.com>
2018-12-21 00:49:07 -0500

    Init day 21

---
+++ src/2018/day21/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using common;
+using day16;
+
+namespace day21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines;
+            using(var reader = new InputReader("test.txt"))
+            {
+                lines = reader.GetLines().ToList();
+            }
+        }
+    }
+}
